c28d6a8987a7f1591ff4e03007b0a9b8b2503e4b
Andrey <andr.belousow2017@yandex.ru>
2020-09-27 23:16:35 +0300

    Застрял на отображении колонок

--- js/start.js
+++ js/start.js
@@ -4,28 +4,37 @@ const CLOUD_WIDTH = 420;
 const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
-const GAP = 10;
-const FONT_GAP = 30;
-// const TEXT_WIDTH = 50;
+const CLOUD_GAP = 10;
+const BAR_HEIGHT = 150;
+const BAR_WIDTH = 350;
+const BAR_X = CLOUD_X + 40;
+const BAR_Y = CLOUD_Y + 60;
+const FONT_GAP = 20;
+const COLUMN_GAP = 50;
+const CONGRATULATION_TEXT = [`Ура вы победили!`, `Список результатов:`];
 const TEXT_HEIGHT = 20;
-const BAR_WIDTH = 20;
-const barHeight = CLOUD_HEIGHT - GAP - TEXT_HEIGHT;
+const COLUMN_WIDTH = 40;
+const columnHeight = BAR_HEIGHT - TEXT_HEIGHT - TEXT_HEIGHT;
 
 let renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-let getMaxElement = function (arr) {
-
+let getMaxElement = function (times) {
+  return times.reduce((accumulator, timesItem) => {
+    return Math.round(Math.max(accumulator, timesItem));
+  });
 };
 
 window.renderStatistics = function (ctx, names, times) {
+  const max = getMaxElement(times);
+
   renderCloud(
       ctx,
-      CLOUD_X + GAP,
-      CLOUD_Y + GAP,
-      `rgba(0, 0, 0, 0.7 )`
+      CLOUD_X + CLOUD_GAP,
+      CLOUD_Y + CLOUD_GAP,
+      `rgba(0, 0, 0, 0.7)`
   );
   renderCloud(
       ctx,
@@ -34,17 +43,23 @@ window.renderStatistics = function (ctx, names, times) {
       `#fff`
   );
 
+  CONGRATULATION_TEXT.forEach((textItem, index) => {
+    ctx.fillStyle = `#000`;
+    ctx.fillText(textItem, CLOUD_X + FONT_GAP, CLOUD_Y + CLOUD_GAP + FONT_GAP + (FONT_GAP * index));
+    ctx.font = `16px PT Mono`;
+  });
   ctx.fillStyle = `#000`;
+  ctx.fillRect(BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
 
-  ctx.fillText(
-      `Ура вы победили!`,
-      CLOUD_X + GAP,
-      CLOUD_Y + GAP
-  );
+  ctx.fillStyle = `#FFF`;
+  ctx.textBaseline = `top`;
+  ctx.fillText(times[0], BAR_X, BAR_Y);
+  ctx.rotate((90 * Math.PI) / 180);
+  ctx.font = `16px PT Mono`;
+  ctx.fillStyle = `red`;
+  ctx.fillRect(BAR_X, BAR_Y + TEXT_HEIGHT, COLUMN_WIDTH, (columnHeight * times[0]) / max);
 
-  ctx.fillText(
-      `Список результатов:`,
-      CLOUD_X + GAP,
-      CLOUD_Y + FONT_GAP
-  );
+  // names.forEach((player, index) => {
+  //   ctx.fillText(player, );
+  // });
 };

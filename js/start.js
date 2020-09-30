807a9e0199315b28f6840431a3d0b24ae492f7a6
Andrey <andr.belousow2017@yandex.ru>
2020-09-30 22:26:54 +0300

    Есть базовый функционал

--- js/start.js
+++ js/start.js
@@ -9,12 +9,14 @@ const BAR_HEIGHT = 150;
 const BAR_WIDTH = 350;
 const BAR_X = CLOUD_X + 40;
 const BAR_Y = CLOUD_Y + 60;
-const FONT_GAP = 20;
+const STRING_GAP = 20;
+const TEXT_GAP = 15;
 const COLUMN_GAP = 50;
 const CONGRATULATION_TEXT = [`Ура вы победили!`, `Список результатов:`];
 const TEXT_HEIGHT = 20;
 const COLUMN_WIDTH = 40;
-const columnHeight = BAR_HEIGHT - TEXT_HEIGHT - TEXT_HEIGHT - CLOUD_GAP - FONT_GAP;
+const COLUMN_HEIGHT_CONSTANT = 105;
+let playerColor;
 
 let renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -27,9 +29,12 @@ let getMaxElement = function (times) {
   });
 };
 
+let getRandomColor = function() {
+  return Math.random().toFixed(1) * 100;
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  const max = getMaxElement(times);
-  // const max = 4881;
+  let max = getMaxElement(times);
 
   renderCloud(
       ctx,
@@ -46,24 +51,38 @@ window.renderStatistics = function (ctx, names, times) {
 
   CONGRATULATION_TEXT.forEach((textItem, index) => {
     ctx.fillStyle = `#000`;
-    ctx.fillText(textItem, CLOUD_X + FONT_GAP, CLOUD_Y + CLOUD_GAP + FONT_GAP + (FONT_GAP * index));
+    ctx.fillText(textItem, CLOUD_X + STRING_GAP, CLOUD_Y + CLOUD_GAP + STRING_GAP + (STRING_GAP * index));
     ctx.font = `16px PT Mono`;
   });
-  ctx.fillStyle = `#000`;
+  ctx.fillStyle = `#FFF`;
   ctx.fillRect(BAR_X, BAR_Y, BAR_WIDTH, BAR_HEIGHT);
 
-  ctx.fillStyle = `#FFF`;
-  ctx.textBaseline = `top`;
-  ctx.fillText(times[0], BAR_X, BAR_Y);
-  ctx.font = `16px PT Mono`;
-  ctx.fillStyle = `red`;
-  let tempHeight = Math.round((columnHeight * times[0]) / max);
-  // let tempHeight = 83;
-  // ctx.fillRect(BAR_X, BAR_Y + 50, COLUMN_WIDTH, 105 - 50);
-  ctx.fillRect(BAR_X, BAR_Y + (BAR_HEIGHT - tempHeight), COLUMN_WIDTH, 105 - (BAR_HEIGHT - tempHeight));
-  // console.log(tempHeight, BAR_HEIGHT - Math.round((columnHeight * times[0]) / max), Math.round(times[0]), max);
+  times.forEach((timeItem, index) => {
+    let calculatedHeight = (COLUMN_HEIGHT_CONSTANT * Math.round(timeItem)) / max;
 
-  // names.forEach((player, index) => {
-  //   ctx.fillText(player, );
-  // });
+    if (names[index] === `Вы`) {
+      playerColor = `rgba(255, 0, 0, 1)`;
+    } else {
+      playerColor = `hsl(240, ${getRandomColor().toString()}%, 50%)`;
+    }
+    ctx.fillStyle = playerColor;
+    ctx.fillRect(
+      BAR_X + (COLUMN_WIDTH + COLUMN_GAP) * index,
+      BAR_Y + (BAR_HEIGHT - calculatedHeight),
+      COLUMN_WIDTH,
+      COLUMN_HEIGHT_CONSTANT - (BAR_HEIGHT - calculatedHeight)
+    );
+    ctx.fillStyle = `#000`;
+    ctx.textBaseline = `middle`;
+    ctx.fillText(
+      Math.round(timeItem),
+      BAR_X + (COLUMN_WIDTH + COLUMN_GAP) * index,
+      BAR_Y + (BAR_HEIGHT - calculatedHeight) - TEXT_GAP
+    );
+    ctx.fillText(
+      names[index],
+      BAR_X + (COLUMN_WIDTH + COLUMN_GAP) * index,
+      BAR_Y + COLUMN_HEIGHT_CONSTANT + TEXT_GAP
+    );
+  });
 };
